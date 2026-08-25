d1202d642f842c8f74edc0a9355e47d7ceea140a
taskmaster682 <sayedanwarulhaq@gmail.com>
2026-08-25 22:44:00 +0530

    ghjf3

--- src/models/User.js
+++ src/models/User.js
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema({
   },
   role: {
     type: String,
-    enum: ['Super Admin', 'Content Manager', 'Support', 'User'],
+    enum: ['Super Admin', 'Content Manager', 'Support', 'Nerpat', 'User'],
     default: 'User',
   },
   active: {
